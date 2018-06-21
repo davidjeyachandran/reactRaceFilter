d548a579d25a58bb7ffb97475f0af7844dbafa35
David Jeyachandran <davidjeyachandran@gmail.com>
2018-06-21 18:01:03 -0500

    Add 3 levels of filters put text filter

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ class InputFilter extends Component {
     super(props)
     this.state = {
       inputValue: '',
-      distValue: ''
+      distValue: '',
+      categoriaValue: '',
+      sexoValue: ''
     }
     this.changeHandlerInput = this.changeHandlerInput.bind(this)
     this.changeHandlerDist = this.changeHandlerDist.bind(this)
+    this.changeHandlerCategoria = this.changeHandlerCategoria.bind(this)
+    this.changeHandlerSexo = this.changeHandlerSexo.bind(this)
+    this.clickHandler = this.clickHandler.bind(this)
   }
 
   changeHandlerInput(event) {
@@ -22,36 +27,50 @@ class InputFilter extends Component {
     this.props.filterFunction({distValue: event.target.value});
   }
 
+  changeHandlerCategoria(event) {
+    this.setState({categoriaValue: event.target.value})
+    this.props.filterFunction({categoriaValue: event.target.value});
+  }
+
+  changeHandlerSexo(event) {
+    this.setState({sexoValue: event.target.value})
+    this.props.filterFunction({sexoValue: event.target.value});
+  }
+
+  clickHandler() {
+    this.setState({inputValue: '', distValue: '', categoriaValue: ''})
+    this.props.filterFunction({inputValue: '', distValue: '', categoriaValue: ''});
+  }
+
   render() {
     return (
       <div>
-        <input value={this.state.inputValue} onChange={this.changeHandlerInput} />
         <select id="select_distancia" className="select-options form-control" onChange={this.changeHandlerDist}>
             <option key="0" value="">--Elegir la Distancia --</option>
           {this.props.distancia.map(item =>
             <option key={item} value={item}>{item}</option>
             )}
         </select>
-        <select id="select_categoria" className="select-options form-control">
+        <select id="select_categoria" className="select-options form-control" onChange={this.changeHandlerCategoria}>
+            <option key="0" value="">--Elegir la Categoría --</option>
           {this.props.categorias.map(item =>
-            <option key={item} value="{item}">{item}</option>
+            <option key={item} value={item}>{item}</option>
             )}
         </select>
+        <select id="select_sexo" className="select-options form-control" onChange={this.changeHandlerSexo}>
+            <option key="0" value="">--Elegir el Sexo --</option>
+          {this.props.sexos.map(item =>
+            <option key={item} value={item}>{item}</option>
+            )}
+        </select>
+        <input value={this.state.inputValue} onChange={this.changeHandlerInput} placeholder="Search number or name..." />
+        <button onClick={this.clickHandler}>Reset</button>
       </div>
       )
   }
 }
 
 const ShowData = ({data}) => {
-  let html = data.map((item) => {
-    return <tr key={item.Num}>
-      <td>{item.Num}</td>
-      <td>{item.Nombres}</td>
-      <td>{item.Apellidos}</td>
-      <td>{item.Dist}</td>
-      <td>{item.Categoria}</td>
-      <td>{item.Time}</td></tr>
-  })
   return (
     <table>
       <thead>
@@ -61,11 +80,22 @@ const ShowData = ({data}) => {
           <th>Lastname</th> 
           <th>Distancia</th>
           <th>Categoria</th>
+          <th>Sexo</th>
           <th>Time</th>
         </tr>
       </thead>
       <tbody>
-        {html}
+        {data.map((item) => 
+          <tr key={item.Num}>
+            <td>{item.Num}</td>
+            <td>{item.Nombres}</td>
+            <td>{item.Apellidos}</td>
+            <td>{item.Dist}</td>
+            <td>{item.Categoria}</td>
+            <td>{item.Sexo}</td>
+            <td>{item.Time}</td>
+          </tr>
+        )}
       </tbody>
     </table>
     )
@@ -77,7 +107,11 @@ class Results extends Component {
     this.state = {
       data: [],
       inputValue: '',
-      distValue: ''
+      distValue: '',
+      categoriaValue: '',
+      sexoValue: '',
+      distList: [],
+      categoriaList: []
     }
     this.getFilterValue = this.getFilterValue.bind(this)
   }
@@ -96,6 +130,8 @@ class Results extends Component {
   getFilterValue(values) {
     if (values.hasOwnProperty('inputValue')) this.setState({inputValue: values['inputValue']});
     if (values.hasOwnProperty('distValue')) this.setState({distValue: values['distValue']});
+    if (values.hasOwnProperty('categoriaValue')) this.setState({categoriaValue: values['categoriaValue']});
+    if (values.hasOwnProperty('sexoValue')) this.setState({sexoValue: values['sexoValue']});
   }
 
   getUniqueList(list, key) {
@@ -103,23 +139,28 @@ class Results extends Component {
   }
 
   render() {
-      console.log(this.state.distValue, this.state.inputValue)
+
       let filteredList = this.state.data.filter(item => {
-            let name = (item.Nombres + ' ' + item.Apellidos).toLowerCase()
-            return (item.Dist === this.state.distValue || this.state.distValue === '') &&
-            (name.indexOf(this.state.inputValue.toLowerCase()) >= 0 || this.state.inputValue === '')
+            let name = item.Num + ' ' + (item.Nombres + ' ' + item.Apellidos).toLowerCase()
+            return (
+              (item.Dist === this.state.distValue || this.state.distValue === '') &&
+              (item.Categoria === this.state.categoriaValue || this.state.categoriaValue === '') &&
+              (item.Sexo === this.state.sexoValue || this.state.sexoValue === '') &&
+              (name.indexOf(this.state.inputValue.toLowerCase()) >= 0 || this.state.inputValue === '')
+              )
             }
           )
-          .slice(0, 100)
+          // .slice(0, 100)
 
     return (
       <div>
-        <h1>Race Example</h1>
         <InputFilter 
           filterFunction={this.getFilterValue}
           distancia={this.getUniqueList(this.state.data, 'Dist')}
-          categorias={this.getUniqueList(filteredList, 'Categoria')}
+          categorias={this.getUniqueList(this.state.data, 'Categoria')}
+          sexos={this.getUniqueList(this.state.data, 'Sexo')}
         />
+        <p>Count: {filteredList.length}</p>
         <ShowData data={filteredList} />
       </div>
       )
@@ -135,6 +176,7 @@ class App extends Component {
     return (
       <div>
         <div className="right">
+          <h1>Daddy's Race Tool</h1>
           <Results endpoint={filename} />
         </div>
       </div>
